ae3858ad6a354d125cdaa80e38e588b8ad19834e
Jasmina <rustamovajasmina20@gmail.com>
2024-03-29 12:33:37 +0500

    section 8 ni qo'shdim

---
+++ js/main.js
@@ -0,0 +1,88 @@
+function hide(elementId) {
+    let list = document.getElementById(elementId);
+    if (list.style.display === "none") {
+        list.style.display = "block";
+    } else {
+        list.style.display = "none";
+    }
+}
+function hide1(elementId) {
+    let list1 = document.getElementById(elementId);
+    if (list1.style.display === "none") {
+        list1.style.display = "block";
+    } else {
+        list1.style.display = "none";
+    }
+}
+function hide2(elementId) {
+    let list2 = document.getElementById(elementId);
+    if (list2.style.display === "none") {
+        list2.style.display = "block";
+    } else {
+        list2.style.display = "none";
+    }
+}
+function hide3(elementId) {
+    let list3 = document.getElementById(elementId);
+    if (list3.style.display === "none") {
+        list3.style.display = "block";
+    } else {
+        list3.style.display = "none";
+    }
+}
+function hide4(elementId) {
+    let list4 = document.getElementById(elementId);
+    if (list4.style.display === "none") {
+        list4.style.display = "block";
+    } else {
+        list4.style.display = "none";
+    }
+}
+function hide5(elementId) {
+    let list5 = document.getElementById(elementId);
+    if (list5.style.display === "none") {
+        list5.style.display = "block";
+    } else {
+        list5.style.display = "none";
+    }
+}
+function hide6(elementId) {
+    let list6 = document.getElementById(elementId);
+    if (list6.style.display === "none") {
+        list6.style.display = "block";
+    } else {
+        list6.style.display = "none";
+    }
+}
+function hide7(elementId) {
+    let list7 = document.getElementById(elementId);
+    if (list7.style.display === "none") {
+        list7.style.display = "block";
+    } else {
+        list7.style.display = "none";
+    }
+}
+function hide8(elementId) {
+    let list8 = document.getElementById(elementId);
+    if (list8.style.display === "none") {
+        list8.style.display = "block";
+    } else {
+        list8.style.display = "none";
+    }
+}
+function hide9(elementId) {
+    let list9 = document.getElementById(elementId);
+    if (list9.style.display === "none") {
+        list9.style.display = "block";
+    } else {
+        list9.style.display = "none";
+    }
+}
+function hide10(elementId) {
+    let list10 = document.getElementById(elementId);
+    if (list10.style.display === "none") {
+        list10.style.display = "block";
+    } else {
+        list10.style.display = "none";
+    }
+}
